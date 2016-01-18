d70cb7745255cc16fe689e9341874a2f6188ef97
guigui64 <guillaume.comte10@gmail.com>
2016-01-18 11:40:36 +0100

    stack

--- sandbox.adb
+++ sandbox.adb
@@ -1,6 +1,38 @@
 with GenericStack;
+with Text_IO;
 
 procedure Sandbox is
+
+   package IntegerStack is new GenericStack (Elt => Integer);
+   use IntegerStack;
+   The_Stack : Stack;
+   Interm    : Integer;
+
 begin
-   null;
+
+   declare
+      Toto : Integer;
+   begin
+      Toto := Pop (The_Stack);
+   exception
+      when Empty_Stack =>
+         Text_IO.Put_Line ("Empty stack !");
+      when others =>
+         Text_IO.Put_Line ("Unexpected error !");
+   end;
+   Put (The_Stack, 6);
+   Put (The_Stack, 4);
+   Interm := Pop (The_Stack);
+   Text_IO.Put_Line (Interm'Img);
+   Interm := Pop (The_Stack);
+   Text_IO.Put_Line (Interm'Img);
+   Interm := Pop (The_Stack);
+   Text_IO.Put_Line (Interm'Img);
+   Text_IO.Put_Line ("Unreachable ?");
+
+exception
+
+   when others =>
+      Text_IO.Put_Line ("Exception !!!");
+
 end Sandbox;
